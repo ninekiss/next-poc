5a6436d136e2cadaf89bc35b7bbefbf0d195fad2
nk-noreply <nk-noreply@gmail.com>
2023-08-24 23:40:39 +0800

    feat: Route Groups

---
+++ app/(user)/collection/layout.tsx
@@ -0,0 +1,12 @@
+export default function CollectionLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <h1>Collection Layout</h1>
+      {children}
+    </div>
+  )
+}
---
+++ app/(user)/layout.tsx
@@ -0,0 +1,12 @@
+export default function SettingLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <h1>Setting Layout</h1>
+      {children}
+    </div>
+  )
+}
